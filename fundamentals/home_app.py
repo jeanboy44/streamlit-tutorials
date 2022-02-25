b16789ec686df479459df22d99a01169a3709091
jeanboy <eesidoro44@gmail.com>
2022-02-26 00:38:47 +0900

    initial commit

---
+++ fundamentals/home_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+from .text_inputs_app import main as text_input
+
+PRFX = "fundamentals_home"
+
+
+def main():
+    st.title("Fundamentals")
+
+    menu = ["Home", "TextInputs"]
+    choice = st.sidebar.selectbox("List", menu, key=f"{PRFX}_cases")
+
+    if choice == "Home":
+        st.subheader("Home")
+
+    elif choice == "TextInputs":
+        text_input()
+    elif choice == "ML":
+        pass
+    else:
+        st.subheader("About")
+
+
+if __name__ == "__main__":
+    main()
